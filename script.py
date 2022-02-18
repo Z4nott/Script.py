54bb5d7f4fa6838952e0fc76c54ef60f7f4ec61d
Augusto Zanott <49215017+Z4nott@users.noreply.github.com>
2022-02-18 16:17:48 -0300

    Create script.py
    Primeiro script em PY

---
+++ script.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Script
+
+nome = input('Digite o seu nome: ')
+print('Olá', nome, '!', 'Prazer em te conhecer!')
+dia = input('Digite o dia do seu nascimento: ')
+mes = input('Digite o mes do seu nascimento: ')
+ano = input('Digite o ano do seu nascimento: ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano + '.')
+
+n1 = input('Digite somente numeros agora, aperte Enter! ')
+# Print ('Digite somente numero, aperte enter')
+n2 = int(input('Digite o primeiro numero: '))
+n3 = int(input('Digite o segundo numero: '))
+n4 = (input('Atenção, não é permitido a divisão por zero!'))
+print('A soma é', n2/n3)
